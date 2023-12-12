975cb564afb6ae19623dfce44ab0e53fde299b3c
lexmin0412 <zhangle_dev@outlook.com>
2023-12-13 00:22:05 +0800

    feat(where-am-i): add getOS

---
+++ packages/where-am-i/src/get-os.ts
@@ -0,0 +1,34 @@
+/**
+ * 获取当前操作系统
+ * @param appSign 需要区分的软件特有代码 如wldxm
+ */
+export const getOS = (appSign?: string) => {
+	let ua = window.navigator.userAgent.toLowerCase()
+	if (!appSign) {
+		if (ua.indexOf('android') > -1) {
+			return 'android'
+		}
+		else if (ua.indexOf('iphone') > -1 || ua.indexOf('ipad') > -1) {
+			return 'ios'
+		}
+		else {
+			return null
+		}
+	}
+	else {
+		if (ua.indexOf(appSign) == -1) {
+			return null
+		}
+		if (ua.indexOf('android') > -1) {
+			return 'android'
+		}
+		else if (ua.indexOf('iphone') > -1 || ua.indexOf('ipad') > -1) {
+			return 'ios'
+		}
+		else {
+			return null
+		}
+	}
+}
+
+export default getOS
--- packages/where-am-i/src/index.ts
+++ packages/where-am-i/src/index.ts
@@ -1,1 +1,2 @@
 export * from './is-mobile'
+export * from './get-os'
