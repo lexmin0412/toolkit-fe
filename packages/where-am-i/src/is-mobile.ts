e56d109127f5bb9250873447104fe9ca4ea696fc
lexmin0412 <zhangle_dev@outlook.com>
2023-12-13 20:57:43 +0800

    style(where-am-i): format file

--- packages/where-am-i/src/is-mobile.ts
+++ packages/where-am-i/src/is-mobile.ts
@@ -2,7 +2,7 @@
  * 判断当前是否是移动设备
  */
 export const isMobile = (): boolean => {
-    const sUserAgent = window.navigator.userAgent.toLowerCase();
-    const isMobile = /ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(sUserAgent);
-    return isMobile;
+	const sUserAgent = window.navigator.userAgent.toLowerCase();
+	const isMobile = /ipad|iphone|midp|rv:1.2.3.4|ucweb|android|windows ce|windows mobile/.test(sUserAgent);
+	return isMobile;
 };
